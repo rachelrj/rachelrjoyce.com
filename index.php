dc1813f48d2d26609b84851265e25efcb975ea54
Rachel <connect@rachelrjoyce.com>
2015-10-19 20:07:33 -0600

    Add Google Anaytics script

--- index.php
+++ index.php
@@ -32,6 +32,18 @@
 <!-- Include MY Stylesheet -->
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-69054407-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </HEAD>
 
 <BODY>	  
